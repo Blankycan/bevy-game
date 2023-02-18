fa0a860b13295b84223602d9ffcd2c7f550618ae
Blankycan <blankycan@gmail.com>
2023-02-18 22:42:28 +0100

    Got 3D sprite working, and camera zoom and rotation.

--- src/character.rs
+++ src/character.rs
@@ -1,34 +1,180 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
+use bevy_sprite3d::{AtlasSprite3d, AtlasSprite3dComponent, Sprite3dParams};
+
+use crate::{ImageAssets, GameState, GameSystemLabel};
+
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            // Register types
+            .register_type::<TurnTowardCamera>()
+            // On enter
+            .add_system_set(
+                SystemSet::on_enter(GameState::Playing)
+                    .with_system(spawn_player.label(GameSystemLabel::Player))
+            )
+            // On update
+            .add_system_set(
+                SystemSet::on_update(GameState::Playing)
+                    .with_system(animate_sprite_system)
+                    .with_system(turning_toward_camera)
+                    .with_system(control_player)
+            );
+    }
+}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    //mut meshes: ResMut<Assets<Mesh>>,
+    //mut materials: ResMut<Assets<StandardMaterial>>,
+    images: Res<ImageAssets>,
+    mut sprite_params: Sprite3dParams,
+) {
+    commands
+        .spawn(AtlasSprite3d {
+            atlas: images.character_sheet.clone(),
+            pixels_per_metre: 16.,
+            partial_alpha: true,
+            unlit: true,
+            index: 12,
+            pivot: Some(Vec2::new(0.5, 0.0)),
+            transform: Transform::from_xyz(1.0, 0.0, 2.0),
+
+            ..default()
+        }
+        .bundle(&mut sprite_params))
+        .insert(Name::new("2D Player"))
+        .insert(Player)
+        .insert(CharacterBundle {
+            ..default()
+        });
+    /*
+    commands
+    .spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad {
+                size: Vec2::new(1.0, 2.0),
+                ..default()
+            })),
+            material: materials.add(Color::WHITE.into()),
+            transform: Transform::from_xyz(1.0, 1.0, 2.0),
+            ..default()
+        })
+        .insert(Name::new("2D Player"))
+        .insert(CharacterBundle {
+            turn_to_camera: TurnTowardCamera(true),
+            ..default()
+        })
+        .insert(
+            AtlasSprite3d {
+                atlas: images.character_sheet.clone(),
+                
+                pixels_per_metre: 32.,
+                partial_alpha: true,
+                unlit: true,
+                
+                index: 3,
+                
+                ..default()
+            }
+            .bundle(&mut sprite_params),
+        );
+        */
+}
+
+fn control_player (
+    mut player_query: Query<&mut Transform, With<Player>>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>
+) {
+    let mut transform = player_query.single_mut();
+    let mut direction = Vec2::splat(0.0);
+
+    if keyboard.pressed(KeyCode::W) {
+        direction.y -= 1.0;
+    }
+    if keyboard.pressed(KeyCode::S) {
+        direction.y += 1.0;
+    }
+    if keyboard.pressed(KeyCode::A) {
+        direction.x -= 1.0;
+    }
+    if keyboard.pressed(KeyCode::D) {
+        direction.x += 1.0;
+    }
+    
+    transform.translation.x += direction.x * 1.0 * time.delta_seconds();
+    transform.translation.z += direction.y * 1.0 * time.delta_seconds();
+}
+
+#[derive(Component)]
+pub struct Player;
+
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
 pub struct TurnTowardCamera(pub bool);
 
 pub fn turning_toward_camera(
-  mut query: Query<(&TurnTowardCamera, &mut Transform)>,
-  camera: Query<&Transform, (With<Camera>, Without<TurnTowardCamera>)>,
-  time: Res<Time>
+    mut query: Query<(&TurnTowardCamera, &mut Transform)>,
+    camera: Query<&Transform, (With<Camera>, Without<TurnTowardCamera>)>,
+    time: Res<Time>,
 ) {
-  let camera = camera.single();
-  for (should_turn, mut obj_transform) in &mut query {
-    if should_turn.0 == true {
-      let new_rotation = obj_transform.looking_at(camera.translation, Vec3::Y).rotation * Quat::from_euler(EulerRot::XYZ, 0.0, 3.14, 0.0);
-      let delta_rotation = Quat::slerp(obj_transform.rotation, new_rotation, time.delta_seconds() * 5.0);
-      obj_transform.rotation.y = delta_rotation.y;
+    let camera = camera.single();
+    for (should_turn, mut obj_transform) in &mut query {
+        if should_turn.0 == true {
+            let mut look_position = camera.translation - obj_transform.translation;
+            look_position.y = 0.0;
+            let rotation = Transform::IDENTITY.looking_at(look_position, Vec3::Y).rotation;
+            obj_transform.rotation = obj_transform.rotation.slerp(rotation, time.delta_seconds() * 10.0);
+        }
     }
-  }
 }
 
+enum Direction {
+    Down,
+    Left,
+    Up,
+    Right
+}
+
+#[derive(Component)]
+struct AnimatedCharacter {
+    direction: Direction,
+
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite_system(
+    time: Res<Time>,
+    mut query: Query<(&mut AnimationTimer, &mut AtlasSprite3dComponent)>,
+) {
+    for (mut timer, mut sprite) in query.iter_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = (sprite.index + 1) % sprite.atlas.len();
+        }
+    }
+}
 
 #[derive(Bundle)]
-pub struct CharacterBundle {
-    pub turn_to_camera: TurnTowardCamera
+struct CharacterBundle {
+    turn_to_camera: TurnTowardCamera,
+    //sprite: AtlasSprite3d,
+    animate_timer: AnimationTimer,
 }
 
 impl Default for CharacterBundle {
     fn default() -> Self {
         Self {
-            turn_to_camera: TurnTowardCamera(true)
+            turn_to_camera: TurnTowardCamera(true),
+            animate_timer: AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,195 @@
-use bevy::prelude::*;
+use std::f32::{NEG_INFINITY, INFINITY};
+
+use bevy::{prelude::*, input::mouse::{MouseWheel, MouseMotion}};
+use bevy_asset_loader::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_sprite3d::Sprite3dPlugin;
 
 mod character;
-use character::{CharacterBundle, TurnTowardCamera, turning_toward_camera};
+use crate::character::{PlayerPlugin, Player};
 
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+enum GameState {
+    Loading,
+    Playing,
+}
 
+#[derive(SystemLabel)]
+pub enum GameSystemLabel {
+    Player,
+    Camera
+}
+
+#[derive(AssetCollection, Resource)]
+pub struct ImageAssets {
+    #[asset(texture_atlas(tile_size_x = 16.0, tile_size_y = 16.0))]
+    #[asset(texture_atlas(columns = 9, rows = 3))]
+    #[asset(path = "Characters.png")]
+    character_sheet: Handle<TextureAtlas>,
+}
+
+fn main() {
+    println!("Starting Bevy app..");
+    App::new()
+        .add_loading_state(
+            LoadingState::new(GameState::Loading)
+                .continue_to_state(GameState::Playing)
+                .with_collection::<ImageAssets>(),
+        )
+        .add_state(GameState::Loading)
+        .insert_resource(ClearColor(Color::rgb(0.16, 0.16, 0.16)))
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        // Inspector
+        .add_plugin(WorldInspectorPlugin)
+        // Other plugins
+        .add_plugin(Sprite3dPlugin)
+        // Our systems
+        .add_startup_system(spawn_camera)
+        .register_type::<FollowCamera>()
+        .add_startup_system(spawn_basic_scene)
+        .add_plugin(PlayerPlugin)
+        .add_system_set(
+            SystemSet::on_update(GameState::Playing)
+                .with_system(camera_follow)
+                .with_system(camera_control)
+        )
+        .run();
+}
+
+#[derive(Component, Reflect)]
+struct FollowCamera {
+    // Offset to the center point of the target
+    offset: Vec3,
+    // Distance towards target
+    zoom: f32,
+    zoom_speed: f32,
+    zoom_limit_min: f32,
+    zoom_limit_max: f32,
+    // Used to smooth the movement and rotation
+    speed_transition: f32,
+    // Used to rotate around the followed object
+    rotation_horizontal: f32,
+    rotation_horizontal_speed: f32,
+    rotation_horizontal_limit_min: f32,
+    rotation_horizontal_limit_max: f32,
+    // Used to rotate up and down around the followed object
+    rotation_vertical: f32,
+    rotation_vertical_speed: f32,
+    rotation_vertical_limit_min: f32,
+    rotation_vertical_limit_max: f32
+}
+
+impl Default for FollowCamera {
+    fn default() -> Self {
+        Self {
+            offset: Vec3::ZERO,
+            zoom: 8.0,
+            zoom_speed: 30.0,
+            zoom_limit_min: 2.0,
+            zoom_limit_max: 18.0,
+            speed_transition: 10.0,
+            rotation_horizontal: 0.2,
+            rotation_horizontal_speed: 0.5,
+            rotation_horizontal_limit_min: NEG_INFINITY,
+            rotation_horizontal_limit_max: INFINITY,
+            rotation_vertical: -0.6,
+            rotation_vertical_speed: 0.3,
+            rotation_vertical_limit_min: -1.2,
+            rotation_vertical_limit_max: -0.05
+        }
+    }
+}
 
 fn spawn_camera(mut commands: Commands) {
+    let camera_transform = Transform::from_xyz(2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y);
+
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: camera_transform,
         ..default()
-    });
+    })
+    .insert(FollowCamera {
+        offset: Vec3::new(0.0, 0.32, 0.0),
+        ..default()
+    })
+    .insert(Name::new("Follow Camera"));
+}
+
+fn camera_follow(
+    mut camera_query: Query<(&mut Transform, &FollowCamera), (With<FollowCamera>, Without<Player>)>,
+    player_query: Query<&Transform, (With<Player>, Without<Camera>)>,
+    time: Res<Time>
+) {
+    let player_transform = player_query.single();
+    let (mut camera_transform, follow_camera) = camera_query.single_mut();
+
+    let rot_hor = Quat::from_axis_angle(Vec3::Y, follow_camera.rotation_horizontal);
+    let rot_ver = Quat::from_axis_angle(Vec3::X, follow_camera.rotation_vertical);
+    let target_rotation = rot_hor * rot_ver;
+    let target_position = target_rotation.mul_vec3(Vec3::Z * follow_camera.zoom) + player_transform.translation + follow_camera.offset;
+    camera_transform.rotation = camera_transform.rotation.lerp(target_rotation, follow_camera.speed_transition * time.delta_seconds());
+    camera_transform.translation = camera_transform.translation.lerp(target_position, follow_camera.speed_transition * time.delta_seconds());
+}
+
+fn camera_control(
+    mut camera_query: Query<&mut FollowCamera, With<FollowCamera>>,
+    keyboard: Res<Input<KeyCode>>,
+    mouse: Res<Input<MouseButton>>,
+    mut scroll_evr: EventReader<MouseWheel>,
+    mut motion_evr: EventReader<MouseMotion>,
+    time: Res<Time>
+) {
+    use bevy::input::mouse::MouseScrollUnit;
+    let mut follow_camera = camera_query.single_mut();
+
+    // Scroll
+    for ev in scroll_evr.iter() {
+        match ev.unit {
+            MouseScrollUnit::Line => {
+                follow_camera.zoom -= ev.y * follow_camera.zoom_speed * time.delta_seconds();
+                follow_camera.zoom = follow_camera.zoom.clamp(follow_camera.zoom_limit_min, follow_camera.zoom_limit_max);
+            }
+            MouseScrollUnit::Pixel => {
+                follow_camera.zoom -= ev.y * follow_camera.zoom_speed * time.delta_seconds();
+                follow_camera.zoom = follow_camera.zoom.clamp(follow_camera.zoom_limit_min, follow_camera.zoom_limit_max);
+            }
+        }
+    }
+
+    // Rotate
+    if mouse.pressed(MouseButton::Middle) || (mouse.pressed(MouseButton::Right) && keyboard.any_pressed([KeyCode::LShift, KeyCode::RShift])) {
+        for ev in motion_evr.iter() {
+            follow_camera.rotation_horizontal -= ev.delta.x * follow_camera.rotation_horizontal_speed * time.delta_seconds();
+            follow_camera.rotation_horizontal = follow_camera.rotation_horizontal.clamp(follow_camera.rotation_horizontal_limit_min, follow_camera.rotation_horizontal_limit_max);
+            follow_camera.rotation_vertical += ev.delta.y * follow_camera.rotation_vertical_speed * time.delta_seconds();
+            follow_camera.rotation_vertical = follow_camera.rotation_vertical.clamp(follow_camera.rotation_vertical_limit_min, follow_camera.rotation_vertical_limit_max);
+        }
+    }
 }
 
 fn spawn_basic_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>
 ) {
-    // Check out https://github.com/FraserLee/bevy_sprite3d
-    let texture_handle = asset_server.load("Characters.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 22.0), 8, 12, None, None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-    // Texture material for player
-    let material_handle = materials.add(StandardMaterial {
-        alpha_mode: AlphaMode::Blend,
-        unlit: true,
-        ..default()
-    });
-
     // Ground plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
-        material: materials.add(Color::rgb(0.4, 0.8, 0.4).into()),
-        ..default()
-    })
-    .insert(Name::new("Ground"));
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Capsule {
-            radius: 0.5,
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+            material: materials.add(Color::rgb(0.4, 0.8, 0.4).into()),
             ..default()
-        })),
-        material: materials.add(Color::rgb(0.4, 0.45, 0.8).into()),
-        transform: Transform::from_xyz(0.0, 1.0, 0.0),
-        ..default()
-    })
-    .insert(Name::new("Capsule"));
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Quad {
-            size: Vec2::new(1.0, 2.0),
-            ..default()
-        })),
-        material: material_handle,
-        transform: Transform::from_xyz(1.0, 1.0, 2.0),
-        ..default()
-    })
-    .insert(Name::new("2D Player"))
-    .insert(CharacterBundle {
-        turn_to_camera: TurnTowardCamera(true),
-        ..default()
-    })
-    .insert(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle,
-        transform: Transform::from_scale(Vec3::splat(6.0)),
-        ..default()
-    })
-    .insert(AnimationTimer(
-        Timer::from_seconds(0.1, TimerMode::Repeating)
-    ));
+        })
+        .insert(Name::new("Ground"));
 
     // Light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                intensity: 1500.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
             ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    })
-    .insert(Name::new("Point Light"));
-}
-
-#[derive(Component, Deref, DerefMut)]
-struct AnimationTimer(Timer);
-
-fn animate_sprite(
-    time: Res<Time>,
-    texture_atlases: Res<Assets<TextureAtlas>>,
-    mut query: Query<(
-        &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-        &Handle<TextureAtlas>
-    )>
-) {
-    for (mut timer, mut sprite, texture_atlas_handle) in &mut query {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
-        }
-    }
-}
-
-fn main() {
-    println!("Starting Bevy app..");
-    App::new()
-        .insert_resource(ClearColor(Color::rgb(0.16, 0.16, 0.16)))
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        // Inspector
-        .add_plugin(WorldInspectorPlugin)
-        .register_type::<TurnTowardCamera>()
-        // Our systems
-        .add_startup_system(spawn_basic_scene)
-        .add_startup_system(spawn_camera)
-        .add_system(turning_toward_camera)
-        .add_system(animate_sprite)
-        .run();
+        })
+        .insert(Name::new("Point Light"));
 }
